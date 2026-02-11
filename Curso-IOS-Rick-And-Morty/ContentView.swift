1575f9b58107ad9eb14936fa5e5b9dc478278338
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-02-11 11:56:01 +0100

    Initial Commit

---
+++ Curso-IOS-Rick-And-Morty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curso-IOS-Rick-And-Morty
+//
+//  Created by Equipo 8 on 11/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curso-IOS-Rick-And-Morty/Curso_IOS_Rick_And_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_IOS_Rick_And_MortyApp.swift
+//  Curso-IOS-Rick-And-Morty
+//
+//  Created by Equipo 8 on 11/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_IOS_Rick_And_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

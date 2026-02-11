d5f7437d5e9716c584706fca84319327111d7ff0
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-02-11 14:54:20 +0100

    actualizados Modelo, ApiService y VistaDetallePersonaje

---
+++ Curso-IOS-Rick-And-Morty/Vistas/VistaDetallePersonaje.swift
@@ -0,0 +1,18 @@
+//
+//  VistaDetallePersonaje.swift
+//  Curso-IOS-Rick-And-Morty
+//
+//  Created by Equipo 8 on 11/2/26.
+//
+
+import SwiftUI
+
+struct VistaDetallePersonaje: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaDetallePersonaje()
+}
